7c025e9675a0305c539c592dc4374c46425b8a36
code-with-ameen <contact.developer.ameen@gmail.com>
2026-05-27 21:54:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -597,7 +597,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // BACKEND REQUEST (NOT FORMSPREE)
             // ==================================================
             const response = await fetch(
-                'https://portfolio-mail-backend-77z4.onrender.com/contact',
+                'https://new-backend.up.railway.app/contact',
                 {
                     method: 'POST',
                     headers: {
