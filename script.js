3f579daf3eb95b19e1945db1a6b896ea9c635abe
code-with-ameen <contact.developer.ameen@gmail.com>
2026-05-27 20:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -597,7 +597,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // BACKEND REQUEST (NOT FORMSPREE)
             // ==================================================
             const response = await fetch(
-                'https://portfolio-mail-backend-77z4.onrender.com',
+                'https://portfolio-mail-backend-77z4.onrender.com/contact',
                 {
                     method: 'POST',
                     headers: {
